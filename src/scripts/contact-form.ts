37b39acb16e25b5627489f10c056a9114c5b72ab
Suryansh <suryanshmishra.0904@gmail.com>
2025-08-01 15:05:57 +0530

    feat: add contact form triggers

---
+++ src/scripts/contact-form.ts
@@ -0,0 +1,62 @@
+export function initContactFormTriggers() {
+  const formTriggers = [
+    ...document.querySelectorAll('.contact-form-trigger'),
+  ] as HTMLElement[];
+
+  const closeTriggers = [
+    ...document.querySelectorAll('.close-contact-form-trigger'),
+  ] as HTMLElement[];
+
+  const skipToBrochureDownload = [
+    ...document.querySelectorAll('.skip-to-download-brochure'),
+  ] as HTMLElement[];
+
+  formTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      showForm();
+    });
+  });
+
+  closeTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      hideForm();
+    });
+  });
+
+  skipToBrochureDownload.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault();
+      skipToDownloadBrochure();
+    });
+  });
+}
+
+export function showForm() {
+  const form = document.getElementById('contact-form-modal') as HTMLFormElement;
+
+  form.classList.remove('hidden');
+  form.classList.add('flex');
+  document.body.style.overflow = 'hidden';
+}
+
+export function hideForm() {
+  const form = document.getElementById('contact-form-modal') as HTMLFormElement;
+  form.classList.remove('flex');
+  form.classList.add('hidden');
+  document.body.style.overflow = '';
+}
+
+export function skipToDownloadBrochure() {
+  console.log('We reached here');
+  hideForm();
+  downloadBrochure();
+}
+
+export function downloadBrochure() {
+  window.open(
+    'https://www.hindalco.com/upload/pdf/totalis-brochure.pdf',
+    '_blank'
+  );
+}
